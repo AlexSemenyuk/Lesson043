4e1eb39eb0bb32cb19cabb8eb9bee3906b5cf004
AlexSemenyuk <Semen_jf87@student.itstep.org>
2022-12-21 15:08:43 +0200

    ready for check

--- Lesson043.sql
+++ Lesson043.sql
@@ -1,6 +1,6 @@
 /*
 * Java. Lesson043. Task01
-* Створити бд movies
+* Створити БД movies
 * Semenyuk Alexander
 * Date 17.12.2022
 * Створити бд movies
@@ -17,8 +17,7 @@ CREATE TABLE movies
     rating       int,
     plot         text,
     length       int,
-    director_id  int                            not null,
-    gente_id     int                            not null
+    director_id  int                            not null
 );
 CREATE TABLE directors
 (
@@ -28,6 +27,10 @@ CREATE TABLE directors
     nationality varchar(50),
     birth_date  date
 );
+alter table movies
+    add constraint movies_directors_fk
+        foreign key (director_id) references directors (id);
+
 CREATE TABLE actors
 (
     id          int primary key auto_increment not null,
@@ -38,6 +41,45 @@ CREATE TABLE actors
 );
 CREATE TABLE genres
 (
-    id          int primary key auto_increment not null,
-    name        varchar(50) unique             not null
+    id   int primary key auto_increment not null,
+    name varchar(50) unique             not null
 );
+
+create table movies_genres
+(
+    movie_id int not null unique,
+    genre_id int not null unique,
+    primary key (movie_id, genre_id)
+);
+
+alter table movies_genres
+    add constraint movies_genres_movies_fk
+        foreign key (movie_id) references movies (id),
+    add constraint movies_genres_genres_fk
+        foreign key (genre_id) references genres (id);
+
+create table movies_actors
+(
+    movie_id int not null unique,
+    actor_id int not null unique,
+    primary key (movie_id, actor_id)
+);
+
+alter table movies_actors
+    add constraint movies_actors_movies_fk
+        foreign key (movie_id) references movies (id),
+    add constraint movies_actors_actors_fk
+        foreign key (actor_id) references actors (id);
+/*
+drop table movies;
+drop table movies_genres;
+drop table movies_actors;
+drop table actors;
+drop table genres;
+drop table directors;
+drop database myDataBase;
+
+alter table movies
+drop constraint movies_directors_fk;
+*/
+
